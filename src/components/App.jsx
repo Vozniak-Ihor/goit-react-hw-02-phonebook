82ad073b30e7322d8c8caca166010f5edee47f3b
Vozniak-Ihor <118538389+Vozniak-Ihor@users.noreply.github.com>
2023-05-31 22:15:06 +0300

    логіка submit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,58 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import LoginForm  from './LogForm/LoginForm';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+  handleSubmit = (values, actions) => {
+    const contactId = nanoid();
+    if (this.state.contacts.some(item => item.name === values.name)) {
+      alert(`${values.name} is already in contacts`);
+      return
+    }  
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, { 
+        id: contactId, 
+        name: values.name, 
+        number: values.phoneNumber.toString()
+      }]
+    }));
+    console.log(this.state.contacts);
+    actions.resetForm()
+  }
+  // handleSubmit = (values, actions) =>
+  // {
+  //       const contactId = nanoid()
+  //         if (this.state.contacts.some(item => item.name === values.name))
+  //           {
+  //           alert(`${values.name} is already in contacts`)
+  //           return
+  //           }  
+  //     this.setState((prevState) =>
+  //     (
+  //       {
+  //         contacts: [...prevState.contacts,
+  //         { id: contactId , name: values.name, number: values.number.toString()}]
+  //       }
+  //       )
+  //       )
+  //       console.log(this.state.contacts);
+  //       actions.resetForm()
+  // }
+  
+  render() {
+    return (
+      <>
+        <LoginForm onSubmit={this.handleSubmit} contacts={ this.state.contacts}/>
+      </>
+    );
+  }
+}
---
+++ src/components/LogForm/LoginForm.jsx
@@ -0,0 +1,57 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+// import { FormContainer, FormLabelName, FormInputName, FormInputTel,  FormButton, ErrorText } from "./LodForm.styled";
+
+const schema = yup.object().shape({
+  name: yup
+    .string()
+    .required("Ім'я обов'язкове"),
+  phoneNumber: yup
+    .number()
+    .min(10)
+    .required("Номер телефону обов'язковий"),
+});
+
+const initialValues = {
+  name: '',
+  phoneNumber: '',
+};
+class LoginForm extends Component{
+
+render(){
+
+    return (
+      <Formik
+        initialValues={initialValues}
+        onSubmit={this.props.onSubmit}
+        validationSchema={schema}
+      >
+        <Form autoComplete="off">
+          <label>
+            Name
+            <Field type="text" name="name" />
+            <ErrorMessage name="name" component="div" />
+          </label>
+  
+          <label>
+            Number
+            <Field type="tel" name="phoneNumber" />
+            <ErrorMessage name="phoneNumber" component="div" />
+          </label>
+          <button type="submit">Add contact</button>
+        </Form>
+      </Formik>
+    );
+}
+};
+
+export default LoginForm
+
+LoginForm.propTypes = {  onSubmit: PropTypes.func.isRequired,
+  initialValues: PropTypes.exact({
+    name: PropTypes.string.isRequired,
+    phoneNumber: PropTypes.string.isRequired,
+  }),
+};
